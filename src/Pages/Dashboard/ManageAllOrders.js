62ce734b4f9b557633359791ddc68eee4e56203d
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-06-05 23:18:59 +0600

    admin id-pass updated

--- src/Pages/Dashboard/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AllOrders from './AllOrders';
 
 const ManageAllOrders = () => {
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/orders')
+        .then(res => res.json())
+        .then(data => setOrders(data))
+    }, []);
     return (
         <div>
-            <h2 className='text-2xl text-center font-bold mb-6'>Manage All Orders</h2>
-            
+            <h2 className='text-2xl text-center font-bold mb-6'>Manage All Orders: {orders.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Product Id</th>
+                            <th>Transaction Id</th>
+                            <th>Quantity</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <AllOrders key={order._id} order={order}/>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
